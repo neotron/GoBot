17a398ce826e72520b53f8b39b0a954a91f731d5
David Hedbor <david@ookla.com>
2019-02-15 22:53:17 +0100

    Channel id command

--- core/dispatch/handlers/id.go
+++ core/dispatch/handlers/id.go
@@ -18,24 +18,33 @@ func init() {
 	dispatch.Register(&ident{},
 		[]dispatch.MessageCommand{
 			{"id", "Return Discord ID for the user, or all @mentioned users"},
+			{"cid", "Return Discord channel id for the current channel."},
 		},
 		nil, false)
 }
 
 func (*ident) HandleCommand(m *dispatch.Message) bool {
-	var identities []string
-	addAuthor := func(user *discordgo.User) {
-		identities = append(identities, fmt.Sprintf("%v has id %s", user.Username, user.ID))
-	}
-	if len(m.Args) == 0 {
-		addAuthor(m.Message.Author)
-	} else {
-		funk.ForEach(m.Message.Mentions, addAuthor)
-	}
-	if len(identities) > 0 {
-		m.ReplyToChannel("Identities:\n\t%s", strings.Join(identities, "\n\t"))
-	} else {
-		m.ReplyToChannel("No one was identified")
+	switch m.Command {
+	case "id":
+		var identities []string
+		addAuthor := func(user *discordgo.User) {
+			identities = append(identities, fmt.Sprintf("%v has id %s", user.Username, user.ID))
+		}
+		if len(m.Args) == 0 {
+			addAuthor(m.Message.Author)
+		} else {
+			funk.ForEach(m.Message.Mentions, addAuthor)
+		}
+		if len(identities) > 0 {
+			m.ReplyToChannel("Identities:\n\t%s", strings.Join(identities, "\n\t"))
+		} else {
+			m.ReplyToChannel("No one was identified")
+		}
+		break
+	case "cid":
+		m.ReplyToChannel("Channel id is %s.", m.ChannelID);
+		break;
+
 	}
 	return true
 }
